2f074a8f984057e384c0fd051b4f79d82baee5d8
Kaliukh Diana <dianakaliukh09@gmail.com>
2026-05-07 13:58:04 +0300

    lab5,6

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -5,3 +5,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('site_django.urls')),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+
+# Цей блок обов'язково має бути в кінці файлу
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
